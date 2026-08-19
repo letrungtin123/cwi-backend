1ee7183c6e472c581bfd2f543deb62833101c3a4
Tin Le <lttvn917@gmail.com>
2026-08-20 02:46:02 +0700

    fix regex

--- src/modules/admin/adminRoutes.ts
+++ src/modules/admin/adminRoutes.ts
@@ -5,7 +5,7 @@ import { HttpError } from '../../http/errors.js'
 import type { AuthService } from '../auth/authService.js'
 import type { PgAdminRepository } from './adminRepository.js'
 
-const uuidPattern = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{12}$/i
+const uuidPattern = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
 const validStatuses = new Set(['part1_only', 'part2_refused_privacy', 'full_private_report'])
 
 function parseLimit(value: unknown) {
